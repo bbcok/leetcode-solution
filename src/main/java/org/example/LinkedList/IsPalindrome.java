c31e3fd87717c80ef5eb82b8069990be695e187a
HOME\luoxukang <luoxukang@inspur.com>
2024-02-07 16:22:14 +0800

    recover

--- src/main/java/org/example/LinkedList/IsPalindrome.java
+++ src/main/java/org/example/LinkedList/IsPalindrome.java
@@ -16,6 +16,22 @@ public class IsPalindrome {
         treaverse(node.next);
         b=b+node.val;
     }
+    int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                // 维护 nums[0..slow] 无重复
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        // 数组长度为索引 + 1
+        return slow + 1;
+    }
 
     public static void main(String[] args) {
         int [] a={1,2};
