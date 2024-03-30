d4c4ca22835ee8762f8467f51d24cfda31eb65b5
HOME\luoxukang <luoxukang@inspur.com>
2024-03-30 15:38:02 +0800

    add

---
+++ src/main/java/org/example/OD/MaxMale.java
@@ -0,0 +1,2 @@
+package org.example.OD;public class MaxMale {
+}
---
+++ src/main/java/org/example/OD/RobotMaze.java
@@ -0,0 +1,7 @@
+package org.example.OD;
+
+public class RobotMaze {
+    public static void main(String[] args) {
+        
+    }
+}
